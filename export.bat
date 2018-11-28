2261ca35126fb2a8cfe8cbed474d42e823b46ebf
tomatosum <tomatosum@gmail.com>
2018-11-28 11:02:42 +0900

    [update] 設定を分離
    - APIキーをauth.batからの読み込みに変更(サンプルとして `_auth.bat` を追加し、 `auth.bat` は `.gitignore` に登録)
    - BacklogのスペースID、対象プロジェクト、出力ディレクトリはexport.batの引数として受けるように変更

--- export.bat
+++ export.bat
@@ -1,5 +1,19 @@
+@echo off
+setlocal
+
+rem パラメタの読み込み
+set spaceId=%1
+set projectKey=%2
+rem src以下で実行するので出力ディレクトリは1階層上げておく
+set outputDir=..\%3
+
+rem APIキーの読み込み
+call auth.bat
+
 cd src
 powershell Set-ExecutionPolicy RemoteSigned
-powershell .\backlogExport.ps1
+powershell .\backlogExport.ps1 %spaceId% %projectKey% %API_KEY% %outputDir%
 powershell Set-ExecutionPolicy Restricted
 cd ..\
+endlocal
+@echo on
